dd357a13854fdb8d296e5ae8e0f0f725807a111d
amdazm05 <amdali1998@yahoo.com>
2024-07-15 00:23:20 +0100

    cmake: CMake refactor
    Signed-off-by: amdazm05 <amdali1998@yahoo.com>

--- cmake/libcurl/extract_lib_curl.cmake
+++ cmake/libcurl/extract_lib_curl.cmake
@@ -1,7 +1,4 @@
 function(extract_libcurl)
-    if(LIBCURL_FOUND)
-        return()
-    endif()
     message(STATUS "Extracting libcurl...")
     execute_process(COMMAND ${CMAKE_COMMAND} -E tar xzf "${CMAKE_CURRENT_BINARY_DIR}/curl.tar.gz"
                    WORKING_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}")

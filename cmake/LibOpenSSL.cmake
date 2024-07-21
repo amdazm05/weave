f413ec1624e743cbb2e5d80eb383246cbc1296bf
amdazm05 <amdali1998@yahoo.com>
2024-07-22 00:14:36 +0100

    cmake: libcurl and openssl integerated and compile
    Signed-off-by: amdazm05 <amdali1998@yahoo.com>

--- cmake/LibOpenSSL.cmake
+++ cmake/LibOpenSSL.cmake
@@ -1,14 +1,13 @@
 function(LibOpenSSL OPENSSL_VERSION)
     set(OPENSSL_SOURCE_DIR ${CMAKE_CURRENT_BINARY_DIR}/openssl-src) # default path by CMake
     set(OPENSSL_INSTALL_DIR ${CMAKE_CURRENT_BINARY_DIR}/openssl)
-    set(OPENSSL_INCLUDE_DIR ${OPENSSL_INSTALL_DIR}/include)
+    set(OPENSSL_INCLUDE_DIR ${OPENSSL_INSTAL_DIR}/include)
     set(OPENSSL_CONFIGURE_COMMAND ${OPENSSL_SOURCE_DIR}/config)
     include(ExternalProject)
     ExternalProject_Add(
         OpenSSL
         SOURCE_DIR ${OPENSSL_SOURCE_DIR}
-        GIT_REPOSITORY https://github.com/openssl/openssl.git
-        GIT_TAG openssl-3.3.1
+        URL https://github.com/openssl/openssl/releases/download/openssl-${OPENSSL_VERSION}/openssl-${OPENSSL_VERSION}.tar.gz
         USES_TERMINAL_DOWNLOAD TRUE
         CONFIGURE_COMMAND
             ${OPENSSL_CONFIGURE_COMMAND}
@@ -19,7 +18,7 @@ function(LibOpenSSL OPENSSL_VERSION)
         INSTALL_COMMAND make install
         INSTALL_DIR ${OPENSSL_INSTALL_DIR}
         UPDATE_COMMAND ""
-
+        DOWNLOAD_EXTRACT_TIMESTAMP true
     )
 endfunction(LibOpenSSL)
 # Read this :: ->

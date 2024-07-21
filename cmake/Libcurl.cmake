0c46897abf4d77e836d36166ded2bbd4cb240623
amdazm05 <amdali1998@yahoo.com>
2024-07-21 01:37:06 +0100

    cmake: libcurl refactor cmake ext
    Signed-off-by: amdazm05 <amdali1998@yahoo.com>

--- cmake/Libcurl.cmake
+++ cmake/Libcurl.cmake
@@ -1,32 +1,8 @@
-function(LibCurlFetchInstall VERSION)
-    if(LIBCURL_FOUND)
-        message("LIBCURL_FOUND - Skipping")
-        return()
-    endif()
-    set(TEMP ${CMAKE_MODULE_PATH})
-    set(CMAKE_MODULE_PATH ${CMAKE_SOURCE_DIR}/cmake/libcurl)
-    include(fetch_lib_curl)
-    include(extract_lib_curl)
-    set(LIBCURL_INSTALL_DIR ${CMAKE_BINARY_DIR}/libcurl)
-
-    fetch_lib_curl(${VERSION})
-    extract_libcurl()
-    # Configure libcurl
-    execute_process(
-        COMMAND ./configure --with-openssl --prefix=${LIBCURL_INSTALL_DIR}
-        WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/curl-${VERSION}
-    )
-
-    # Build libcurl
-    execute_process(
-        COMMAND make -j4
-        WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/curl-${VERSION}
-    )
-
-    # Install libcurl
-    execute_process(
-        COMMAND make install
-        WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/curl-${VERSION}
-    )
-    set(CMAKE_MODULE_PATH ${TEMP})
-endfunction(LibCurlFetchInstall VERSION)
+function(LibCurlFetchInstall LIBCURL_VERSION)
+    include(FetchContent)
+    FetchContent_Declare(libcurl 
+        GIT_REPOSITORY https://github.com/curl/curl.git
+        GIT_TAG "curl-${LIBCURL_VERSION}")
+    FetchContent_GetProperties(libcurl)
+    FetchContent_MakeAvailable(libcurl)
+endfunction(LibCurlFetchInstall)
--- cmake/libcurl/extract_lib_curl.cmake
+++
@@ -1,5 +0,0 @@
-function(extract_libcurl)
-    message(STATUS "Extracting libcurl...")
-    execute_process(COMMAND ${CMAKE_COMMAND} -E tar xzf "${CMAKE_CURRENT_BINARY_DIR}/curl.tar.gz"
-                   WORKING_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}")
-endfunction(extract_libcurl)

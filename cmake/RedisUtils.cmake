33bb38585764ad9f7f282c0bb085d28dc6a9fed8
amdazm05 <amdali1998@yahoo.com>
2024-08-02 23:37:48 +0100

    cmake: added compile utils for hiredis
    Signed-off-by: amdazm05 <amdali1998@yahoo.com>

--- cmake/RedisUtils.cmake
+++ cmake/RedisUtils.cmake
@@ -15,5 +15,11 @@ macro(LibHIREDIS HIREDIS_VERSION)
     BUILD_COMMAND make
     INSTALL_COMMAND make install DESTDIR=${HIREDIS_INSTALL_DIR}
     INSTALL_DIR ${HIREDIS_INSTALL_DIR}
+    DOWNLOAD_EXTRACT_TIMESTAMP TRUE
   )
-endmacro(FetchHIREDIS)
+endmacro(LibHIREDIS)
+
+function(LinkAndIncludeHiredisToExecutable NAME_EXE)
+  target_link_libraries(${NAME_EXE} PRIVATE ${HIREDIS_INSTALL_DIR}/usr/local/lib/libhiredis.a)
+  target_include_directories(${NAME_EXE} PRIVATE ${HIREDIS_INSTALL_DIR}/usr/local/include)
+endfunction(LinkAndIncludeHiredisToExecutable)

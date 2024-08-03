80786917330cdcbab417e6621843d87f43f78834
amdazm05 <amdali1998@yahoo.com>
2024-08-03 23:49:35 +0100

    cmake: added spdlog dep
    Signed-off-by: amdazm05 <amdali1998@yahoo.com>

---
+++ cmake/SpdlogUtils.cmake
@@ -0,0 +1,24 @@
+macro(Libspdlog spdlog_VERSION)
+  set(spdlog_NAME "spdlog")
+  set(spdlog_URL "https://github.com/gabime/spdlog/archive/refs/tags/${spdlog_VERSION}.tar.gz")
+  set(spdlog_SOURCE_DIR "${CMAKE_CURRENT_BINARY_DIR}/${spdlog_NAME}")
+  set(spdlog_BUILD_DIR "${CMAKE_CURRENT_BINARY_DIR}/${spdlog_NAME}-build")
+  set(spdlog_INSTALL_DIR "${CMAKE_CURRENT_BINARY_DIR}/${spdlog_NAME}-install")
+
+  ExternalProject_Add(
+    ${spdlog_NAME}
+    PREFIX ${spdlog_SOURCE_DIR}
+    URL ${spdlog_URL}
+    DOWNLOAD_DIR ${spdlog_SOURCE_DIR}
+    INSTALL_DIR ${spdlog_INSTALL_DIR}
+    CMAKE_ARGS
+        -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>
+        -DSPDLOG_INSTALL=ON
+    DOWNLOAD_EXTRACT_TIMESTAMP TRUE
+  )
+endmacro(Libspdlog)
+
+function(LinkAndIncludespdlogToExecutable NAME_EXE)
+  target_link_libraries(${NAME_EXE} PRIVATE ${spdlog_INSTALL_DIR}//lib/libspdlog.a)
+  target_include_directories(${NAME_EXE} PRIVATE ${spdlog_INSTALL_DIR}/include)
+endfunction(LinkAndIncludespdlogToExecutable)

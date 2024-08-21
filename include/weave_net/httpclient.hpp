7bbc4270965cf7b702e8173f36ed7199227c47d3
amdazm05 <amdali1998@yahoo.com>
2024-08-22 00:08:49 +0100

    constexpr,wip: uri handling in compile time
    Tested some classes for constexpr type evaluation
    to be used for URI handling , this is some RD
    
    Signed-off-by: amdazm05 <amdali1998@yahoo.com>

--- include/weave_net/httpclient.hpp
+++ include/weave_net/httpclient.hpp
@@ -8,25 +8,25 @@ namespace weave
 {
     /****
      *      Components Required  for HTTP component
-     *      URI handling 
+     *      URI handling
      *      Connection Handler
      *      Request Handler
      *      Response Handler
      *      Error Handling
      ***/
-    
+
     using port_t = int;
     /****
-     *      Http = 80: The standard port number for the HTTP (Hypertext Transfer Protocol) is 80. 
+     *      Http = 80: The standard port number for the HTTP (Hypertext Transfer Protocol) is 80.
      *              This is the default port used for unencrypted HTTP connections.
-     *      Https= 443: 
-     *              The standard port number for the HTTPS (Hypertext Transfer Protocol Secure) is 443. 
+     *      Https= 443:
+     *              The standard port number for the HTTPS (Hypertext Transfer Protocol Secure) is 443.
      *              This is the default port used for encrypted HTTP connections over SSL/TLS.
-     *      Unknown= -1: 
-     *              The Unknown value is assigned a value of -1 to represent a protocol that is not recognized 
+     *      Unknown= -1:
+     *              The Unknown value is assigned a value of -1 to represent a protocol that is not recognized
      *              or does not have a standard port number associated with it.
      ***/
-    enum class protocol_t : port_t 
+    enum class protocol_t : port_t
     {
         Http = 80,
         Https = 443,
@@ -53,37 +53,52 @@ namespace weave
      *      Query : ("?")
      *      Fragment :  "#"
      ***/
-    using uri_t  = std::string_view;
-    class UriHandle
+    using uri_t = std::string_view;
+    class UriHandleCT
     {
-        private:
-            uri_t       _uri;
-        public:
-            protocol_t  protocol;
-            authority_t authority;
-            path_t      path;
-            query_t     query;
-        public:
-            UriHandle()=delete;
-            //Parses URI aswell
-            UriHandle(uri_t const uri);
-            UriHandle(UriHandle &&) =default;
-            UriHandle(UriHandle &)  =default;
-            void update(uri_t const uri);
-            ~UriHandle();
-        private:
-            void split();
+    public:
+        constexpr UriHandleCT(uri_t uri) : _uri(uri)
+        {
+            split(_uri, protocol, authority, path);
+        }
+
+        constexpr UriHandleCT(UriHandleCT &&) = default;
+        constexpr UriHandleCT(UriHandleCT &) = default;
+
+        constexpr void update(uri_t uri)
+        {
+            _uri = uri;
+            split(_uri, protocol, authority, path);
+        }
+
+        ~UriHandleCT() = default;
+
+    private:
+        static constexpr void split(
+            uri_t uri, protocol_t &protocol, authority_t authority, path_t path)
+        {
+            if (uri == "")
+            {
+                return;
+            }
+            constexpr auto protocol_str = uri.substr(0, uri.find(":"));
+        }
+
+    public:
+        uri_t _uri;
+        protocol_t protocol;
+        authority_t authority;
+        path_t path;
     };
 
     class HttpClient
     {
-        public:
-            HttpClient(const WeaveEngine & engine_context);
-            ~HttpClient();
-        private:
-            
+    public:
+        HttpClient(const WeaveEngine &engine_context);
+        ~HttpClient();
+
+    private:
     };
 }
-
 
 #endif //_WEAVE_HTTP_CLIENT

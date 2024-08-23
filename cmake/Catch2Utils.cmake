e591bf97b8726d885f4009346ca90638b76de99d
amdazm05 <amdali1998@yahoo.com>
2024-08-24 00:50:13 +0100

    cmake: enabled logs and disabled catch2 benchmarks
    - Observed CMake script hanging in while configuiring
    - Issue was debugged to be catch2 fetch content being
    too slow
    - Enabled the logs for fetch content quiet
    - Disabled benchmark for faster compilation
    
    Signed-off-by: amdazm05 <amdali1998@yahoo.com>

--- cmake/Catch2Utils.cmake
+++ cmake/Catch2Utils.cmake
@@ -1,9 +1,12 @@
 macro(FetchCatch2 CATCH2_VERSION)
     include(FetchContent)
+    Set(FETCHCONTENT_QUIET FALSE)
+    set(CATCH_CONFIG_ENABLE_BENCHMARKING FALSE)
     FetchContent_Declare(
         Catch2
         GIT_REPOSITORY https://github.com/catchorg/Catch2.git
         GIT_TAG        v${CATCH2_VERSION} # or a later release
+        GIT_PROGRESS TRUE
     )
     FetchContent_MakeAvailable(Catch2)
     message("${catch2_SOURCE_DIR}")

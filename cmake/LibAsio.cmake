d08ff8ba63f368bb3416523fccff2721a16f194f
amdazm05 <amdali1998@yahoo.com>
2024-10-17 23:55:51 +0100

    cmake(WIP):Added external project for ASIO
    Signed-off-by: amdazm05 <amdali1998@yahoo.com>

---
+++ cmake/LibAsio.cmake
@@ -0,0 +1,22 @@
+macro(LibAsioFetch ASIO_VERSION)
+    set(ASIO_SOURCE_DIR ${CMAKE_CURRENT_BINARY_DIR}/asio-src)
+    set(ASIO_INSTALL_DIR ${CMAKE_CURRENT_BINARY_DIR}/asio)
+    set(ASIO_INCLUDE_DIR ${ASIO_INSTALL_DIR}/include)
+    ExternalProject_Add(
+        asio
+        GIT_REPOSITORY https://github.com/chriskohlhoff/asio.git
+        GIT_TAG ${ASIO_VERSION}
+        SOURCE_DIR ${ASIO_SOURCE_DIR}
+        USES_TERMINAL_DOWNLOAD TRUE
+        CONFIGURE_COMMAND cd ${ASIO_SOURCE_DIR}/asio && 
+            ./autogen.sh &&
+            ./configure  --with-boost=no
+        BUILD_COMMAND make 
+        INSTALL_COMMAND make install
+        INSTALL_DIR ${ASIO_INSTALL_DIR}
+    )
+endmacro(LibAsioFetch)
+
+function(LibAsioTargetInclude NAME_EXE)
+  target_include_directories(${NAME_EXE} PRIVATE ${LIBCURL_SOURCE_DIR}/include)
+endfunction(LibAsioTargetInclude)
